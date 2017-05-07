d79da0c2d3c52a1d5e66bc7dc9462c74417a8533
Avery Uslaner <uslaner.avery@gmail.com>
2017-05-07 11:40:42 -0600

    Started blog app.

---
+++ cts/blog/models.py
@@ -0,0 +1,13 @@
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+from django.db import models
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
