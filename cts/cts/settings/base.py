03ee1b3cefe812a4d2cd9c3399abfeb467db7763
Avery Uslaner <uslaner.avery@gmail.com>
2017-05-16 22:01:49 -0600

    Added some custom url patterns so I could test out some bootstrap code.

--- cts/cts/settings/base.py
+++ cts/cts/settings/base.py
@@ -144,4 +144,4 @@ WAGTAIL_SITE_NAME = "cts"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'https://conservationtechnologysolutions.com'
