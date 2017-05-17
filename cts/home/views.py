8d558f6e05a7d405f8174f18347875d08b123858
Avery Uslaner <uslaner.avery@gmail.com>
2017-05-17 08:58:59 -0600

    Continued testing of Get Shit Done.

--- cts/home/views.py
+++ cts/home/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
+
+# Testing out the Get Shit Done bootstrap package
 def index(request):
     return render(request, 'home/index.html')
+
+
+def components(request):
+    return render(request, 'home/components.html')
+
+
+def navbar(request):
+    return render(request, 'home/navbar-transparent.html')
+
+
+def notification(request):
+    return render(request, 'home/notification.html')
+
+
+def template(request):
+    return render(request, 'home/template.html')
+
+
+def tutorial(request):
+    return render(request, 'home/tutorial.html')
