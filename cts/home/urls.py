c75b2f95bc586472297f4a731a30c480e7a6ff41
Avery Uslaner <uslaner.avery@gmail.com>
2017-05-17 08:58:59 -0600

    Continued testing of Get Shit Done.

--- cts/home/urls.py
+++ cts/home/urls.py
@@ -5,4 +5,9 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     url(r'^index/', views.index, name='index'),
+    url(r'^components/', views.components, name='components'),
+    url(r'^navbar/', views.navbar, name='navbar'),
+    url(r'^notification/', views.notification, name='notification'),
+    url(r'^template/', views.template, name='template'),
+    url(r'^tutorial/', views.tutorial, name='tutorial'),
 ***REMOVED***
