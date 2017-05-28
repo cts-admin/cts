2c40c4331a1d9c9593d3c514d10036db2b5c3204
Avery Uslaner <uslaner.avery@gmail.com>
2017-05-28 17:30:32 -0600

    Fixed up Contact form and templates.

--- cts/home/forms.py
+++ cts/home/forms.py
@@ -2,8 +2,15 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=70, help_text="Your name")
-    subject = forms.CharField(max_length=100)
-    message = forms.Textarea()
-    sender = forms.EmailField()
+    name = forms.CharField(max_length=70,
+                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}))
+    subject = forms.CharField(max_length=100,
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message'}))
+    sender = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Your email address'}))
     cc_myself = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['cc_myself'].label = "CC Myself"
