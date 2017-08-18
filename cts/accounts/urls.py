9a6f1e6101817fc00b8f495c52f5a4e86124b411
Avery Uslaner <uslaner.avery@gmail.com>
2017-08-18 16:27:36 -0600

    Added urlpatterns for accounts app.

---
+++ cts/accounts/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import include, url
+from registration.backends.model_activation.views import RegistrationView
+from registration.forms import RegistrationFormUniqueEmail
+
+from . import views as account_views
+
+urlpatterns = [
+    url(
+        r'^register/$',
+        RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
+        name='registration_register',
+    ),
+    url(
+        r'^edit/$',
+        account_views.edit_profile,
+        name='edit_profile',
+    ),
+    url(r'^_trac/userinfo/$', account_views.json_user_info),
+    url(r'', include('django.contrib.auth.urls')),
+    url(r'', include('registration.backends.default.urls')),
+]
