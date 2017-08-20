ead3843929b1e32c26ad19ba9697a25c6e8099e5
Avery Uslaner <uslaner.avery@gmail.com>
2017-08-20 16:43:43 -0600

    Refactored account registration template. Created custom templatetag library to aid in custom css additions to template views.

--- cts/cts/settings/base.py
+++ cts/cts/settings/base.py
@@ -83,6 +83,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'home_tags': 'home.templatetags.home_extras',
+            },
         },
     },
 ]
