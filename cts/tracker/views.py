2845b44cee5e4c69ae32e8e5ff87b1ea05a17d05
Avery Uslaner <uslaner.avery@gmail.com>
2017-11-30 12:13:00 -0700

    Implemented initial loading of google map on mapping page.

--- cts/tracker/views.py
+++ cts/tracker/views.py
@@ -1,4 +1,11 @@
+import os
+
 from django.shortcuts import render
 
+MAPS_API_KEY = os.environ.get('MAPS_API_KEY', None)
+
+
 def index(request):
-    return render(request, 'tracker/index.html')
+    return render(request, 'tracker/index.html', {
+        'maps_api_key': MAPS_API_KEY,
+    })
