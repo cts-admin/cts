f8cfb2c7a8ed71fd18241388bd013f45e76502ae
Avery Uslaner <uslaner.avery@gmail.com>
2017-11-30 13:09:11 -0700

    Added code to pass model waypoints from the view to the template.

--- cts/tracker/views.py
+++ cts/tracker/views.py
@@ -1,11 +1,17 @@
 import os
 
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
+
+from tracker.models import Waypoint
 
 MAPS_API_KEY = os.environ.get('MAPS_API_KEY', None)
 
 
 def index(request):
-    return render(request, 'tracker/index.html', {
+    waypoints = Waypoint.objects.all().order_by('name')
+    return render_to_response('tracker/index.html', {
+        'waypoints': waypoints,
         'maps_api_key': MAPS_API_KEY,
+        'content': render_to_string('tracker/waypoints.html', {'waypoints': waypoints}),
     })
