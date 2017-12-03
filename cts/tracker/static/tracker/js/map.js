c97662aa5abf86f5f916b5724e2118e5bba15caa
Avery Uslaner <uslaner.avery@gmail.com>
2017-12-03 15:03:26 -0700

    Google map object can now interact with user created marker locations.

--- cts/tracker/static/tracker/js/map.js
+++ cts/tracker/static/tracker/js/map.js
@@ -1,12 +1,8 @@
 function initMap() {
     var center = {lat: 39.828300, lng: -98.579500};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
         center: center,
-        mapTypeId: 'roadmap',
-    });
-    var marker = new google.maps.Marker({
-        position: center,
-        map: map
+        mapTypeId: 'roadmap'
     });
 }
