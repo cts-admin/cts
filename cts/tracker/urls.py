ddf7758b2d0198cd2256c53e8910643b728423ae
Avery Uslaner <uslaner.avery@gmail.com>
2017-12-03 16:00:25 -0700

    User points can now be repositioned on map and saved to database.

--- cts/tracker/urls.py
+++ cts/tracker/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'tracker'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^save$', views.save, name='save'),
 ]
--- cts/tracker/views.py
+++ cts/tracker/views.py
@@ -1,5 +1,7 @@
 import os
+import simplejson
 
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 
@@ -15,3 +17,17 @@ def index(request):
         'maps_api_key': MAPS_API_KEY,
         'content': render_to_string('tracker/waypoints.html', {'waypoints': waypoints}),
     })
+
+
+def save(request):
+    """
+    Save waypoints.
+    """
+    for waypoint_str in request.POST.get('waypointsPayload', '').splitlines():
+        waypoint_id, waypoint_x, waypoint_y = waypoint_str.split()
+        waypoint = Waypoint.objects.get(id=int(waypoint_id))
+        waypoint.geometry.set_x(float(waypoint_x))
+        waypoint.geometry.set_y(float(waypoint_y))
+        waypoint.save()
+
+    return HttpResponse(simplejson.dumps(dict(isOk=1)), content_type='application/json')
