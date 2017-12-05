a8cd9c55efd868c00b63e697c75ae77968837539
Avery Uslaner <uslaner.avery@gmail.com>
2017-12-04 18:35:36 -0700

    Added ability to convert provisional seed zone shapefile to model objects.

--- cts/tracker/load.py
+++ cts/tracker/load.py
@@ -1,7 +1,7 @@
 import os
 
 from django.contrib.gis.utils import LayerMapping
-from .models import WorldBorder
+from .models import ProvisionalSeedZone, WorldBorder
 
 # Auto-generated `LayerMapping` dictionary for ProvisionalSeedZone model
 provisionalseedzone_mapping = {
@@ -11,6 +11,15 @@ provisionalseedzone_mapping = {
     'perimeter': 'PERIMETER',
     'eco': 'ECO',
     'level3': 'LEVEL3',
+    'level3_nam': 'LEVEL3_NAM',
+    'fid_2017_n': 'FID_2017_N',
+    'tmin_class': 'Tmin_Class',
+    'ahm_class': 'AHM_class',
+    'seed_zone': 'seed_zone',
+    'new_label': 'New_label',
+    'ahm_name': 'AHM_Name',
+    'geom': 'MULTIPOLYGON',
+
 }
 
 world_mapping = {
@@ -32,10 +41,23 @@ world_shp = os.path.abspath(
     os.path.join(os.path.dirname(__file__), 'data', 'TM_WORLD_BORDERS-0.3.shp')
 )
 
+psz_shp = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), 'data/provision_seed_zones',
+                 '2017_New_Labels_LIII_EcoRegions_14June17-colored.shp')
+)
 
-def run(verbose=True):
-    lm = LayerMapping(
+
+def load_psz(verbose=True):
+    psz_lm = LayerMapping(
+        ProvisionalSeedZone, psz_shp, provisionalseedzone_mapping,
+        encoding='iso-8859-1',
+    )
+    psz_lm.save(strict=True, verbose=verbose)
+
+
+def load_wb(verbose=True):
+    wb_lm = LayerMapping(
         WorldBorder, world_shp, world_mapping,
         transform=False, encoding='iso-8859-1',
     )
-    lm.save(strict=True, verbose=verbose)
+    wb_lm.save(strict=True, verbose=verbose)
