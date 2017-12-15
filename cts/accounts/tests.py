d04faec098ec8fc7e69d043e1e7d8746341e97b1
Avery Uslaner <uslaner.avery@gmail.com>
2017-12-14 21:28:40 -0700

    User Profile Test Corrections
    Refactored unittests for user profile views to correct errors arising from the Profile model which expands on the Django user auth model, but is only created by accessing the edit profile view after logging in for the first time. Also added 'testserver' to allowed hosts in settings files involved in testing to prevent failures arising from the use of Django's test Client model.
    
    v0.1 bug-smashing
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- cts/accounts/tests.py
+++ cts/accounts/tests.py
@@ -1,25 +1,37 @@
-from unittest import mock
-
 from django.contrib.auth.models import User
-from django.test import TestCase
-from django_hosts.resolvers import reverse
+from django.shortcuts import reverse
+from django.test import Client, TestCase
+
+from .models import Profile
 
 
 class ViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.credentials = {'username': 'a-user',
+                           'password': 'password',
+                           'email': 'email@email.com'}
+        cls.user = User.objects.create_user(**cls.credentials)
 
-    def setUp(self):
-        self.credentials = {'username': 'a-user', 'password': 'password'}
-        self.user = User.objects.create_user(**self.credentials)
-
-    @mock.patch('accounts.views.get_user_stats')
-    def test_user_profile(self, mock_user_stats):
-        response = self.client.get(reverse('user_profile', host='www', args=['a-user']))
+    def test_user_profile(self):
+        self.assertEqual(User.objects.count(), 1)
+        self.client.login(username='a-user', password='password')
+        # Profile model does not exist until attempting to access edit profile view
+        edit_response = self.client.get(reverse('edit_profile'))
+        self.assertContains(edit_response, 'a-user')
+        self.assertContains(edit_response, 'Edit Your Profile')
+        self.assertEqual(Profile.objects.count(), 1)
+        response = self.client.get(reverse('user_profile', args=['a-user']))
         self.assertContains(response, 'a-user')
-        mock_user_stats.assert_called_once_with(self.user)
+        self.assertContains(response, 'User Profile')
 
     def test_login_redirect(self):
-        response = self.client.post(reverse('login'), self.credentials)
-        self.assertRedirects(response, '/accounts/edit/')
+        response = self.client.post(reverse('login'), {
+            'username': 'a-user',
+            'password': 'password'
+        })
+        self.assertRedirects(response, reverse('edit_profile'))
 
     def test_profile_view_reversal(self):
         """
@@ -27,4 +39,4 @@ class ViewTests(TestCase):
         valid username characters.
         """
         for username in ['asdf', '@asdf', 'asd-f', 'as.df', 'as+df']:
-            reverse('user_profile', host='www', args=[username])
+            reverse('user_profile', args=[username])
