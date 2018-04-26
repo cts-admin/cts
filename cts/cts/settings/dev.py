eb194ae44db729b3cf5498fb5185e1d8a11e0523
Avery Uslaner <uslaner.avery@gmail.com>
2018-04-26 13:26:01 -0600

    Development Settings TLD Correction
    Fixed SSL cert file locations in development settings. Changed from .com
    to .org.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- cts/cts/settings/dev.py
+++ cts/cts/settings/dev.py
@@ -8,8 +8,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'web', '0.0.0.0', '.dev.conservationtechnologysolutions.org',
                  'www.dev.conservationtechnologysolutions.org', '104.131.38.9']
 
-EMAIL_SSL_KEYFILE = '/etc/letsencrypt/live/dev.conservationtechnologysolutions.com/privkey.pem'
-EMAIL_SSL_CERTFILE = '/etc/letsencrypt/live/dev.conservationtechnologysolutions.com/fullchain.pem'
+EMAIL_SSL_KEYFILE = '/etc/letsencrypt/live/dev.conservationtechnologysolutions.org/privkey.pem'
+EMAIL_SSL_CERTFILE = '/etc/letsencrypt/live/dev.conservationtechnologysolutions.org/fullchain.pem'
 
 try:
     from .local import *
