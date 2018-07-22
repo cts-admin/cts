ddc3ed613eb2b7ece03c3dd56f0f1f4236530e4b
Avery Uslaner <uslaner.avery@gmail.com>
2018-07-22 16:24:52 -0600

    Bug Fix: Template Tags Not Loaded
    Loaded wagtailcore_tags in individual member template.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- cts/members/views.py
+++ cts/members/views.py
@@ -19,7 +19,7 @@ class IndividualMemberListView(ListView):
     def get_queryset(self):
         return super().get_queryset().filter(member_until=None)
 
-    def get_context_data(self):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['former_members'] = IndividualMember.objects.filter(
             member_until__lte=timezone_today(),
