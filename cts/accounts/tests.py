e2de3bee5f24ab393905cdcb6418dbc1598f6e6c
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-04-13 13:23:28 -0600

    Registration Workflow Tests
    Added tests for registration workflow to ensure that users were properly created and working activation links were properly sent.
    
    Minor reformatting of activation email and registration complete templates to remove newline characters that could confuse the tests.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- cts/accounts/tests.py
+++ cts/accounts/tests.py
@@ -1,4 +1,7 @@
+import re
+
 from django.contrib.auth import get_user_model
+from django.core import mail
 from django.shortcuts import reverse
 from django.test import Client, TestCase
 
@@ -68,3 +71,53 @@ class ViewTests(TestCase):
         """
         for username in ['asdf', '@asdf', 'asd-f', 'as.df', 'as+df']:
             reverse('user_profile', args=[username])
+
+    def test_user_registration_user(self):
+        """
+        Test the registration workflow creates an inactive user, but not when
+        an existing email address is used.
+        """
+        c = Client()
+        # Should create a new user
+        success_reg_response = c.post(reverse('django_registration_register'),
+                                      {'username': 'another_user',
+                                       'email': 'another_user@email.com',
+                                       'password1': 'secret',
+                                       'password2': 'secret'}, follow=True)
+        print(success_reg_response.content)
+        self.assertContains(success_reg_response,
+                            'An activation link has been sent to the email address you supplied,'
+                            ' along with instructions for activating your account.')
+        self.assertTrue(User.objects.filter(username='another_user').exists())
+        self.assertFalse(User.objects.get(username='another_user').is_active)
+
+        # Should not create a new user
+        fail_reg_response = c.post(reverse('django_registration_register'),
+                                   {'username': 'diff_user_same_email',
+                                    'email': 'another_user@email.com',
+                                    'password1': 'secret',
+                                    'password2': 'secret'}, follow=True)
+        self.assertContains(fail_reg_response, 'This email address is already in use. Please supply a '
+                                                       'different email address.')
+        self.assertFalse(User.objects.filter(username='diff_user_same_email').exists())
+
+    def test_user_registration_email(self):
+        """
+        Test the registration workflow creates an inactive user
+        """
+        c = Client()
+        # Should create a new user
+        c.post(reverse('django_registration_register'),
+               {'username': 'another_user',
+                'email': 'another_user@email.com',
+                'password1': 'secret',
+                'password2': 'secret'}, follow=True)
+        # Test that one message has been sent.
+        self.assertEqual(len(mail.outbox), 1)
+        # Verify that the subject of the first message is correct.
+        self.assertEqual(mail.outbox[0].subject, 'Activate your CTS account')
+        email_body = mail.outbox[0].body
+        activation_link = re.findall("(?P<activation_link>http\S+accounts/activate\S+)",
+                                     email_body)[0]
+        activation_response = c.get(activation_link, follow=True)
+        self.assertContains(activation_response, 'Thanks for signing up! Now you can')
