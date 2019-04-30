5ba654952229941c3cf3c9572912763ba9b5b219
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-04-30 17:17:06 -0600

    Plant Database GIS Admin
    Swapped admin class to use django contribs gis class. Registered the open street map version of the gis admin view.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- cts/plant_database/admin.py
+++ cts/plant_database/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 from .models import Accession, CommonName, Family, Genus, Species, Variety
 
-admin.site.register([Accession, CommonName, Family, Genus, Species, Variety])
+admin.site.register([Accession, CommonName, Family, Genus, Species, Variety], admin.OSMGeoAdmin)
