82a0f236cd939094b9fa17c4e7f6c050f0938255
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-11-15 16:25:09 -0700

    SSL Config Refactor
    Moved SSL related django settings to the production settings file.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- cts/cts/settings/production.py
+++ cts/cts/settings/production.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = ['.conservationtechnologysolutions.org', '167.71.103.48']
 EMAIL_SSL_KEYFILE = '/etc/letsencrypt/live/conservationtechnologysolutions.org/privkey.pem'
 EMAIL_SSL_CERTFILE = '/etc/letsencrypt/live/conservationtechnologysolutions.org/fullchain.pem'
 
+# SSL Stuff
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 try:
     from .local import *
